5c90f5c0679ef1ab884990a6d017a3f7ddb3e025
Matthias Adler <macedigital@users.noreply.github.com>
2017-07-27 23:56:09 +0200

    chore: update git push refspec in `deploy-docs.sh`

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -4,5 +4,5 @@ gitbook build
 cd _book
 git init
 git add -A
-git commit -m 'update book'
-git push -f git@github.com:vuejs-templates/webpack.git master:gh-pages
+git commit -m '(doc): update book'
+git push -f git@github.com:macedigital/vuejs-typescript-webpack.git dev-master:gh-pages
